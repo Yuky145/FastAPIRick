368dc4a25eb8dbb7c06394b64f175fa436eaa46b
Yuky145 <166342706+Yuky145@users.noreply.github.com>
2025-09-24 20:19:56 -0500

    Database with RDS

--- database.py
+++ database.py
@@ -1,21 +1,19 @@
 from sqlmodel import SQLModel, create_engine, Session
+import os
 
-DB_USER = "postgres"
-DB_PASSWORD = "uide.123"
-DB_HOST = "fastapi-db.c762asqgy46c.us-east-2.rds.amazonaws.com"
-DB_PORT = "5432"
-DB_NAME = "fastapi-db"  # o el nombre que le pusiste a la BD
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "uide.123")
+DB_HOST = os.getenv("DB_HOST", "fastapi-db.c762asqgy46c.us-east-2.rds.amazonaws.com")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "fastapi-db")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Crea el motor de conexión
 engine = create_engine(DATABASE_URL, echo=True)
 
-# Función para inicializar la base de datos (crear tablas)
 def init_db():
     SQLModel.metadata.create_all(engine)
 
-# Función para obtener sesión en los endpoints
 def get_session():
     with Session(engine) as session:
         yield session
